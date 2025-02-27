2f709f65e9a6d06f3536ca58a95d90ec80068bd4
Anuja <85613786+AnujaGaikwad@users.noreply.github.com>
2025-02-27 23:11:24 +0530

    Add files via upload
    1. Write a program to store seven fruits in a list entered by the user. 
    2. Write a program to accept marks of 6 students and display them in a sorted 
    manner. 
    3. Check that a tuple type cannot be changed in python. 
    4. Write a program to sum a list with 4 numbers. 
    5. Write a program to count the number of zeros in the following tuple: 
    a = (7, 0, 8, 0, 0, 9)

---
+++ problem2.py
@@ -0,0 +1,16 @@
+marks =[]
+
+m1 =int(input("Enter marks of Student 1: "))
+marks.append(m1)
+m2 =int(input("Enter marks of Student 2: "))
+marks.append(m2)
+m3 =int(input("Enter marks of Student 3: "))
+marks.append(m3)
+m4 =int(input("Enter marks of Student 4: "))
+marks.append(m4)
+m5 =int(input("Enter marks of Student 5: "))
+marks.append(m5)
+m6 =int(input("Enter marks of Student 6: "))
+marks.append(m6)
+marks.sort()
+print(marks)
