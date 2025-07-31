e0c1554c1a1a8242e450a361e0e1eebcea7356d9
Anuja <85613786+AnujaGaikwad@users.noreply.github.com>
2025-07-31 16:50:05 +0530

    Pratice set
    1. Create a class “Programmer” for storing information of few programmers
    working at Microsoft.
    2. Write a class “Calculator” capable of finding square, cube and square root of a
    number.
    3. Create a class with a class attribute a; create an object from it and set ‘a’
    directly using ‘object.a = 0’. Does this change the class attribute?
    4. Add a static method in problem 2, to greet the user with hello.
    5. Write a Class ‘Train’ which has methods to book a ticket, get status (no of seats)
    and get fare information of train running under Indian Railways.
    6. Can you change the self-parameter inside a class to something else (say
    “harry”). Try changing self to “slf” or “harry” and see the effects.

---
+++ 01_problem1.py
@@ -0,0 +1,12 @@
+class Programmer:
+    company = "Microsoft"
+    def __init__(self, name, salary, pin):
+        self.name = name = name
+        self.salary = salary
+        self.pin = pin
+
+
+p = Programmer("Anu", 1200000, 431122)
+print(p.name, p.salary, p.pin)
+r = Programmer("Anjali", 2300000, 431123)
+print(r.name, r.salary, r.pin)
