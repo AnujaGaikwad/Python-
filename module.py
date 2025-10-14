1c997b5889a36e2ce37785985c160ca00c6da953
Anuja <85613786+AnujaGaikwad@users.noreply.github.com>
2025-10-15 02:13:19 +0530

    CHAPTER 12 – ADVANCED PYTHON 1

---
+++ module.py
@@ -0,0 +1,9 @@
+def myFunc():
+    print("Hello anu")
+
+
+if __name__ == "__main__":
+    # If this code is run by its own file
+    print("We are directly running this code")
+    myFunc()
+    print(__name__)
